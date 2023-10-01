0a3ab84e1b569c3d9b1f08a1505747364c3973b1
omkundu <omkundu156@gmail.com>
2023-10-01 15:36:00 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Nav from './Nav';
+import { BrowserRouter } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <Nav/>
      <h1>E-Dashboard</h1>
+     </BrowserRouter>
     </div>
   );
 }
